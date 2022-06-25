1817b5e2fc65da52064fed84f759afb7145d25f9
Titodinelli <timoteo.dinelli@polimi.it>
2022-06-25 12:14:19 +0200

    Update stuff on the examples

---
+++ EXAMPLES/1D_FLAME/DmmLaminarBurningSpeed/LFS/cases/T328/run.sh
@@ -0,0 +1,26 @@
+cd PHI0.6/
+OpenSMOKEpp_PremixedLaminarFlame1D.sh --input input.dic >log &
+cd ../PHI0.7/
+OpenSMOKEpp_PremixedLaminarFlame1D.sh --input input.dic >log &
+cd ../PHI0.8/
+OpenSMOKEpp_PremixedLaminarFlame1D.sh --input input.dic >log &
+cd ../PHI0.9/
+OpenSMOKEpp_PremixedLaminarFlame1D.sh --input input.dic >log &
+cd ../PHI1.0/
+OpenSMOKEpp_PremixedLaminarFlame1D.sh --input input.dic >log &
+cd ../PHI1.1/
+OpenSMOKEpp_PremixedLaminarFlame1D.sh --input input.dic >log &
+cd ../PHI1.2/
+OpenSMOKEpp_PremixedLaminarFlame1D.sh --input input.dic >log &
+cd ../PHI1.3/
+OpenSMOKEpp_PremixedLaminarFlame1D.sh --input input.dic >log &
+cd ../PHI1.4/
+OpenSMOKEpp_PremixedLaminarFlame1D.sh --input input.dic >log &
+cd ../PHI1.5/
+OpenSMOKEpp_PremixedLaminarFlame1D.sh --input input.dic >log &
+cd ../PHI1.6/
+OpenSMOKEpp_PremixedLaminarFlame1D.sh --input input.dic >log &
+cd ../PHI1.7/
+OpenSMOKEpp_PremixedLaminarFlame1D.sh --input input.dic >log &
+cd ../PHI1.85/
+OpenSMOKEpp_PremixedLaminarFlame1D.sh --input input.dic >log &
