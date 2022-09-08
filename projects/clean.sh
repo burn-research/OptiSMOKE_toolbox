77a5a6099c3acd5954bf50ec53985bdd5a3fffa3
timoteo dinelli <timoteo.dinelli@polimi.it>
2022-09-08 14:14:18 +0200

    Removed useless files

--- projects/clean.sh
+++
@@ -1,1 +0,0 @@
-rm -R Linux_BovisaCluster/aclocal.m4  Linux_BovisaCluster/autom4te.cache Linux_BovisaCluster/config.log  Linux_BovisaCluster/config.status  Linux_BovisaCluster/configure Linux_BovisaCluster/depcomp  Linux_BovisaCluster/install-sh  Linux_BovisaCluster/Makefile Linux_BovisaCluster/Makefile.in  Linux_BovisaCluster/missing  Linux_BovisaCluster/OptiSMOKEpp.sh 
