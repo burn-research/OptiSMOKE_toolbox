8a5bb3b27b50f33b1b9df740883dea33c433946b
timoteo dinelli <timoteo.dinelli@polimi.it>
2023-02-23 11:55:15 +0100

    Removed EXAMPLE folder

--- EXAMPLES/1D_FLAME/BurnerStabilizedFlame/clean.sh
+++
@@ -1,1 +0,0 @@
-rm -r BzzFile.txt dakota.rst log OPTIMAL_MECH out* tabular_data.out kinetics Output Output_R1
--- EXAMPLES/direct_measurements/KTP/clean.sh
+++
@@ -1,1 +0,0 @@
-rm -r BzzFile.txt dakota.rst log OPTIMAL_MECH out* tabular_data.dat kinetics err
